f31154bd7216dc03abaadc1faad3e9254d2f2a4d
alexf <alexf@tut.by>
2024-05-30 11:21:39 +0300

    doing

---
+++ theSupermarketQueue.js
@@ -0,0 +1,17 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// const n = 2;
+// let arr2 = [];
+// let k = 0;
+// for (i = 0; i < Math.ceil(arr.length /n); i++) {
+//     arr2.push(arr.slice(k, k + n));
+//     k += n;
+//     }
+// max = 0;
+// result =[];
+//     for (i = 0; i < arr2.length; i++) {
+//     Math.min(arr2[i])     
+
+// }
+
+
+// console.log(arr2)
